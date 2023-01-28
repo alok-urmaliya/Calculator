7b230044ea2c47e01d4cdb7f29e3de8da6209efe
Alok <alokurmaliya0123@gmail.com>
2023-01-28 23:43:45 +0530

    added comments in js, deleted unnecessary code in css

--- calculator.js
+++ calculator.js
@@ -1,6 +1,8 @@
+//configuring the calculator and it's keys 
 const calculator = document.querySelector(".calculator")
 const keys = calculator.querySelector(".calculator__keys")
 
+//code to test if the keys are working
 
 // keys.addEventListener("click", e => {
 //     if (e.target.matches("button")) {
@@ -51,11 +53,12 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action
         const keyContent = key.textContent
         const displayedNum = display.textContent
-
+        //removing is-depressed class from each key
         Array.from(key.parentNode.children)
             .forEach(k => k.classList.remove('is-depressed'))
 
         const previousKeyType = calculator.dataset.previousKeyType
+        //case1 : number key is pressed
         if (!action) {
             if (displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent
@@ -65,6 +68,7 @@ keys.addEventListener('click', e => {
                 display.textContent = displayedNum + keyContent
             }
         }
+        //case 2: decimal key is pressed
         if (action === 'decimal') {
             if (!displayedNum.includes('.')) {
                 display.textContent = displayedNum + keyContent
@@ -74,9 +78,11 @@ keys.addEventListener('click', e => {
                 display.textContent = '0.'
             }
         }
+        //case 3: clear key is pressed
         if (action === 'clear') {
             display.textContent = 0
         }
+        //case 4: any operation key is pressed
         if (action === "add" ||
             action === "subtract" ||
             action === "multiply" ||
@@ -86,6 +92,7 @@ keys.addEventListener('click', e => {
             calculator.dataset.num1 = displayedNum
             calculator.dataset.operator = action
         }
+        // case 5: equals key is pressed
         if (action === 'calculate') {
             const num1 = calculator.dataset.num1
             const operator = calculator.dataset.operator
